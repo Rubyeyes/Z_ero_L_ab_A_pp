81408430ac4e98b7d3c84730da33c63d06716971
Alan Tian <catian315@gmail.com>
2018-09-16 00:11:01 -0700

    add envVariable

---
+++ config/environment/envVariable.js
@@ -0,0 +1,13 @@
+//MongoLab Connection URI
+// process.env['MONGODB_URI']='mongodb://localhost:27017/test';
+process.env['MONGODB_URI']='mongodb://127.0.0.1:27017/zerolabDB';
+
+//Sandgrid Email Setup
+process.env['SANDGRID_USERNAME']='app52741038@heroku.com';
+process.env['SANDGRID_PASSWORD']='rprd70zj0016';
+process.env['SENDGRID_API_KEY']='SG.-9GdjHtrSMGe058CVRI3FA.AmtFt1EXg5pDIV_TYXDdwv-E23kFTur2desggfBuX7QA';
+process.env['WEB_ADDRESS']='admin@example.com';
+// process.env['GOOGLE_API_KEY']='AIzaSyBRn2fNmuY8yyhcB0yltBhRDZ5xbyqPiM4'
+
+// GOOGLE_API_KEY
+process.env['API_KEY'] = 'AIzaSyDbQ1foMHgs47fixIcP9OdH509-zYz--og';
