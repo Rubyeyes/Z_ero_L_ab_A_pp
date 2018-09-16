7c52b51a9c0b6febf0f11983e8a91ec3a2208691
Alan Tian <catian315@gmail.com>
2018-09-15 20:45:38 -0700

    modify banner

--- client/home/home.ctrl.js
+++ client/home/home.ctrl.js
@@ -1,17 +1,25 @@
 angular.module('MyApp')
 	.controller('homeCtrl', ['$scope', '$interval', '$filter', 'WebBanner', 'WebSubBanner', function($scope, $interval, $filter, WebBanner, WebSubBanner) {
 		// webBanner related variable
-		var webBanners = WebBanner.webbanners;
+		var localWebBanners = WebBanner.webbanners;
 		$scope.webSubBanners = WebSubBanner.websubbanners;
-		console.log($scope.webSubBanners)
-		webBanners = $filter('orderBy')(webBanners, 'order');
-		console.log(webBanners);
+		$scope.webBanners = $filter('orderBy')(localWebBanners, 'order');
+		$scope.currentIndex = 0
 
-		var index = 0;
-		$scope.currentBanner = webBanners[index];
-		$interval(showNext, 3000);
+		$scope.currentBanner = $scope.webBanners[$scope.currentIndex];
+		console.log($scope.currentIndex);
+		$interval(showNext, 6000);
 		function showNext(){
-			$scope.currentBanner = webBanners[index++ % webBanners.length];
+			if ($scope.currentIndex == $scope.webBanners.length-1) {
+				$scope.currentIndex = -1;
+			}
+			$scope.currentIndex += 1;
+			$scope.currentBanner = $scope.webBanners[$scope.currentIndex];
+			console.log($scope.currentIndex);
+		}
+		$scope.changeBanner = function(index) {
+			$scope.currentBanner = $scope.webBanners[index]
+			$scope.currentIndex = index
 		}
 		
 	}]);
