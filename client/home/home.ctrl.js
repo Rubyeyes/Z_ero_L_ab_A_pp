c433c3f4eeceafcbd892c77570baa31a8faae83b
Alan Tian <catian315@gmail.com>
2019-12-01 14:56:44 +0800

    add background of banner title

--- client/home/home.ctrl.js
+++ client/home/home.ctrl.js
@@ -2,23 +2,28 @@ angular.module('MyApp')
 	.controller('homeCtrl', ['$scope', '$interval', '$filter', 'WebBanner', 'WebSubBanner', function($scope, $interval, $filter, WebBanner, WebSubBanner) {
 		// webBanner related variable
 		var localWebBanners = WebBanner.webbanners;
-		console.log(localWebBanners);
-		// $scope.webSubBanners = WebSubBanner.websubbanners;
 		$scope.webBanners = $filter('orderBy')(localWebBanners, 'order');
 		$scope.currentIndex = 0
-
 		$scope.currentBanner = $scope.webBanners[$scope.currentIndex];
-		$interval(showNext, 6000);
+
 		function showNext(){
 			if ($scope.currentIndex == $scope.webBanners.length-1) {
 				$scope.currentIndex = -1;
 			}
+
 			$scope.currentIndex += 1;
 			$scope.currentBanner = $scope.webBanners[$scope.currentIndex];
+			$scope.bannerTitleBackground = $scope.currentBanner.title ? "banner_title-background" : null;
+
 		}
+
 		$scope.changeBanner = function(index) {
 			$scope.currentBanner = $scope.webBanners[index]
 			$scope.currentIndex = index
+			$scope.bannerTitleBackground = $scope.currentBanner.title ? "banner_title-background" : null
 		}
 		
+		$interval(() => {
+			showNext();
+		}, 6000);
 	}]);
